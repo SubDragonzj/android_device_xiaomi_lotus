3dfd40d434fdb4063e4017cecfd1b43190dd8266
subdragonzj <zj502407679@gmail.com>
2019-10-24 22:42:36 +0800

    lotus: cover common to lotus

--- patches/install.sh
+++ patches/install.sh
@@ -9,6 +9,6 @@ REPOSITORIES=(
 for repository in "${REPOSITORIES[@]}"; do
     cd "${ROOT}/${repository}"
     echo "${RED}Applying ${NC}${repository} ${RED}patches...${NC}"
-    git am --keep-cr "${ROOT}/device/xiaomi/mt6765-common/patches/${repository}"/*
+    git am --keep-cr "${ROOT}/device/xiaomi/lotus/patches/${repository}"/*
     cd "${ROOT}"
 done
